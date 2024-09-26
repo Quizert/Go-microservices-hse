da3c544ce3cf667759e163ec12e3f4afa91977be
Quizert <81437079+Quizert@users.noreply.github.com>
2024-09-26 22:13:41 +0300

    Delete mylibrary directory

--- mylibrary/library/library.go
+++
@@ -1,9 +0,0 @@
-package library
-
-import "task1/mylibrary/internal/book"
-
-type Library interface {
-	Search(title string) (*book.UserBook, bool)
-	AddBook(book *book.UserBook)
-	generateID(title string) uint32
-}
